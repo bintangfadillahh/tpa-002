873e0677a574b3bb2aceca32b0d38778c28d111f
star2112 <bintangfadel73@gmail.com>
2022-10-09 10:22:44 +0700

    fix

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ submit.addEventListener("submit", (event) => {
   function categories() {
     if (bmi < 18.5) {
       return "Underweight";
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
+    } else if (bmi >= 18.5 && bmi < 25) {
       return "Normal";
-    } else if (bmi >= 25 && bmi <= 29.9) {
+    } else if (bmi >= 25 && bmi < 30) {
       return "Overweight";
     } else if (bmi >= 30) {
       return "Obesity";
